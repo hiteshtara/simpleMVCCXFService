727c1ee38b37d2c4311c37782c92ad71565f597b
evavanya <hht179@gmail.com>
2015-10-22 13:06:30 -0400

    My changes to the repo

--- SampleMVC-CXF-Service/src/main/java/com/luckyryan/sample/service/ExceptionHandler.java
+++ SampleMVC-CXF-Service/src/main/java/com/luckyryan/sample/service/ExceptionHandler.java
@@ -8,7 +8,8 @@ import javax.ws.rs.ext.ExceptionMapper;
 public class ExceptionHandler implements ExceptionMapper<InvalidUserException> {
     public Response toResponse(InvalidUserException exception) {
         Response.Status status;
-
+        pritate static final Logger logger = LoggerFactory (ExceptionHandler.class)
+  //Hitesh making changes to this module 
         status = Response.Status.INTERNAL_SERVER_ERROR;
 
         return Response.status(status).header("exception", exception.getMessage()).build();
